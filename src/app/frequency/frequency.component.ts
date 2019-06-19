db313da05bcb9c692c70eea8f3366e4d16423fbe
Inderjeet <inderbilkhu95@gmail.com>
2019-06-19 21:39:57 +0530

    - Toast service fixed

--- src/app/frequency/frequency.component.ts
+++ src/app/frequency/frequency.component.ts
@@ -7,45 +7,50 @@ import { ToastService } from '../services/toast.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
-	selector: 'frequency',
-	templateUrl: './frequency.component.html',
-	styleUrls: ['./frequency.component.css']
+    selector: 'frequency',
+    templateUrl: './frequency.component.html',
+    styleUrls: ['./frequency.component.css']
 })
 export class FrequencyComponent implements OnInit {
-	frequencyCountForm = new FormGroup({
-		wordCount: new FormControl(1, [Validators.required])
-	});
-	topWords: any;
-	showTopWords: Boolean;
-	displayedColumns = ['index', 'word', 'frequency'];
-	paginator: MatPaginator;
+    frequencyCountForm = new FormGroup({
+        wordCount: new FormControl(1, [Validators.required])
+    });
+    topWords: any;
+    showTopWords: Boolean;
+    displayedColumns = ['index', 'word', 'frequency'];
+    paginator: MatPaginator;
 
-	constructor(private frequencyService: FrequencyService, private toastService: ToastService, private spinner: NgxSpinnerService) { }
+    constructor(
+        private frequencyService: FrequencyService,
+        private toastService: ToastService,
+        private spinner: NgxSpinnerService
+    ) { }
 
-	ngOnInit() {
-		this.showTopWords = false;
-	}
+    ngOnInit() {
+        this.showTopWords = false;
+    }
 
-	onSubmit(formFieldValues) {
-		// this.toastService.showToast(`Getting most frequent ${formFieldValues.wordCount} words...`, 'info');
-		this.showTopWords = false;
-		this.topWords = [];
-		this.spinner.show();
+    onSubmit(formFieldValues) {
+        this.toastService.showToast(`Getting most frequent ${formFieldValues.wordCount} words...`, 'info');
+        this.showTopWords = false;
+        this.topWords = [];
+        this.spinner.show();
 
-		console.log('Words to count:-', formFieldValues);
-		this.frequencyService.getTopWordsFrequency(formFieldValues.wordCount).subscribe(response => {
-			console.log('Response', response);
-			this.showTopWords = true;
-			this.spinner.hide();
+        console.log('Words to count:-', formFieldValues.wordCount);
+        this.frequencyService.getTopWordsFrequency(formFieldValues.wordCount).subscribe(response => {
+            console.log('Response', response);
+            this.spinner.hide();
+            if (response) {
+                this.showTopWords = true;
+                this.toastService.showToast(`Top ${formFieldValues.wordCount} most frequent words generated...`, 'success');
+    
+                this.topWords = new MatTableDataSource(response.data);
+                this.topWords.paginator = this.paginator;
+            }
+        });
+    }
 
-			this.topWords = new MatTableDataSource(response.data);
-			this.topWords.paginator = this.paginator;
-
-			console.log('Top words', this.topWords);
-		});
-	}
-
-	filterWords(value: string) {
-		this.topWords.filter = value.trim().toLowerCase();
-	}
+    filterWords(value: string) {
+        this.topWords.filter = value.trim().toLowerCase();
+    }
 }
--- src/app/services/frequency.service.ts
+++ src/app/services/frequency.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap, map } from 'rxjs/operators';
+import { ToastService } from './toast.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class FrequencyService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient, private toastService: ToastService) { }
 
-  getTopWordsFrequency(wordCount) {
-    return this.http.get(`http://localhost:4005/api/getFrequency/${wordCount}`)
-      .pipe(
-        tap(console.log),
-        map((response: { success: Boolean, data?: any, errors?: any }) => {
-          if (response.success) {
-            return response.data;
-          } else {
-            return response.errors[0];
-          }
-        })
-      )
-      // .subscribe();
-  }
+    getTopWordsFrequency(wordCount) {
+        return this.http.get(`http://localhost:4005/api/getFrequency/${wordCount}`)
+            .pipe(
+                tap(console.log),
+                map((response: { success: Boolean, data?: any, errors?: any }) => {
+                    if (response.success) {
+                        return response.data;
+                    } else {
+                        this.toastService.showToast(`Error: ${response.errors[0]}`, 'danger');
+                        return null;
+                    }
+                })
+            )
+        // .subscribe();
+    }
 }
